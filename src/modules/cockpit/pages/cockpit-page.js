fd49cba508ac66957595861f0166be54f421477d
Ross Dyson <dyson.ross@gmail.com>
2018-12-09 18:00:07 +0900

    connect actions to component

--- src/modules/cockpit/pages/cockpit-page.js
+++ src/modules/cockpit/pages/cockpit-page.js
@@ -2,8 +2,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Local Dependencies
+import {
+    leftKeyDownSignal,
+    leftKeyUpSignal,
+    rightKeyDownSignal,
+    rightKeyUpSignal,
+    upKeyDownSignal,
+    upKeyUpSignal,
+    downKeyDownSignal,
+    downKeyUpSignal
+} from '../actions';
 
 class CockpitPage extends Component {
+    static propTypes = {
+        leftKeyDown: PropTypes.func.isRequired,
+        leftKeyUp: PropTypes.func.isRequired,
+        rightKeyDown: PropTypes.func.isRequired,
+        rightKeyUp: PropTypes.func.isRequired,
+        upKeyDown: PropTypes.func.isRequired,
+        upKeyUp: PropTypes.func.isRequired,
+        downKeyDown: PropTypes.func.isRequired,
+        downKeyUp: PropTypes.func.isRequired
+    };
+
     // constructor(props) {
     //     super(props);
     //
@@ -21,4 +45,16 @@ class CockpitPage extends Component {
     }
 }
 
-export default connect()(CockpitPage);
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({
+        leftKeyDown: leftKeyDownSignal.request,
+        leftKeyUp: leftKeyUpSignal.request,
+        rightKeyDown: rightKeyDownSignal.request,
+        rightKeyUp: rightKeyUpSignal.request,
+        upKeyDown: upKeyDownSignal.request,
+        upKeyUp: upKeyUpSignal.request,
+        downKeyDown: downKeyDownSignal.request,
+        downKeyUp: downKeyUpSignal.request
+    }, dispatch);
+
+export default connect(null, mapDispatchToProps)(CockpitPage);
