9538df7bdc64029e1772c0f63e3ee7bf3253e2a7
Ross Dyson <dyson.ross@gmail.com>
2019-01-06 01:47:26 +0900

    cleaned login

--- src/modules/user/pages/login.js
+++ src/modules/user/pages/login.js
@@ -7,23 +7,54 @@ import { Col, Row } from 'antd';
 
 // Image Dependencies
 import sevenupspot from 'public/img/test/7upspot.gif';
-import contruction from 'public/img/test/construction.gif';
-import H from 'public/img/test/H.gif';
-import I from 'public/img/test/I.gif';
-import newFriends from 'public/img/test/newFriends.gif';
+// import H from 'public/img/test/H.gif';
+// import I from 'public/img/test/I.gif';
+// import newFriends from 'public/img/test/newFriends.gif';
 
 class LoginPage extends Component {
     static propTypes = {};
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: ''
+        };
+    }
+
+    onChange = (e) => {
+        const { value } = e.target;
+
+        this.setState({
+            username: value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { username } = this.state;
+
+        console.log('username', username);
+    };
+
     render() {
+        const { username } = this.state;
+
         return (
-            <Row type="flex" justify="center" style={{ marginTop: '30px' }}>
-                <Col span={20} sm={8}>
-                    <div style={{ textAlign: 'center', marginBottom: '10px' }}>
-                        <img alt="contruction" src={contruction} className="img-responsive" />
-                    </div>
-                    <div className="well" style={{ textAlign: 'center' }}>
-                        <h1>
+            <Row type="flex" justify="center" style={{ height: '100%' }}>
+                <Col span={20} sm={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <div
+                        className="well"
+                        style={{
+                            textAlign: 'center',
+                            marginTop: '-60px',
+                            paddingTop: '40px',
+                            paddingBottom: '40px'
+                        }}
+                    >
+                        <h1 style={{ marginBottom: '40px' }}>
+                            <img alt="7upspot" src={sevenupspot} />
+                            &nbsp;
                             <font color="#FF0000">W</font>
                             <font color="#FF5A00">e</font>
                             <font color="#FFB400">l</font>
@@ -31,41 +62,36 @@ class LoginPage extends Component {
                             <font color="#A5ff00">o</font>
                             <font color="#4Bff00">m</font>
                             <font color="#00ffB4">e</font>
-                        </h1>
-                        <h2>
-                            <img alt="7upspot" src={sevenupspot} />
-                            &nbsp;
-                            to the
                             &nbsp;
                             <img alt="7upspot" src={sevenupspot} />
-                        </h2>
-                        <h1>
-                            <blink>
-                                <font color="#00ffff">H</font>
-                                <font color="#00B4ff">o</font>
-                                <font color="#005Aff">m</font>
-                                <font color="#0000ff">e</font>
-                                <font color="#4B00ff">p</font>
-                                <font color="#A500ff">a</font>
-                                <font color="#FF00ff">g</font>
-                                <font color="#FF00B4">e</font>
-                            </blink>
                         </h1>
+                        <Row type="flex" justify="center">
+                            <Col span={20} sm={10}>
+                                <form className="form-vertical" onSubmit={this.onSubmit}>
+                                    <p style={{ marginBottom: '30px', fontSize: '18px' }}>
+                                        To join, enter username:
+                                    </p>
+                                    <div className="control-group" style={{ marginBottom: '20px' }}>
+                                        <div className="controls">
+                                            <input
+                                                type="text"
+                                                className="input"
+                                                value={username}
+                                                onChange={this.onChange}
+                                                style={{
+                                                    height: '50px',
+                                                    width: '100%',
+                                                    fontSize: '20px',
+                                                    color: 'red'
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn" style={{ width: '100%' }}>Join</button>
+                                </form>
+                            </Col>
+                        </Row>
                     </div>
-                    <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-                        <img alt="H" src={H} />
-                        <img alt="I" src={I} />
-                        &nbsp;
-                        &nbsp;
-                        <img alt="new friends" src={newFriends} />
-                    </div>
-                    <form className="well" style={{ textAlign: 'center' }}>
-
-                        <div>
-                            <input type="text" className="input-large" />
-                        </div>
-                        <button type="submit" className="btn">Enter</button>
-                    </form>
                 </Col>
             </Row>
         );
